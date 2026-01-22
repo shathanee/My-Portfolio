7ab7993d5b761c05058b65ba35fe0360a2428a51
shathanee <shathanee2002@gmail.com>
2026-01-22 12:31:18 +0530

    checked

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowseRouter basename={"/My-Portfolio"}>
     <App />
+    </BrowseRouter>
   </StrictMode>
 );
